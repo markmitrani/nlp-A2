ebd40e9b3cf269b88b4d2efa3f834448a969561e
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2024-04-22 14:33:06 +0200

    finished problem1.py

--- problem1.py
+++ problem1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import nltk
 
 """
 NLP A2: N-Gram Language Models
 
-@author: Klinton Bicknell, Harry Eldridge, Nathan Schneider, Lucia Donatelli, Alexander Koller
+@author: Dany, Markito, El Samo
 
 DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 """
@@ -11,10 +12,14 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 word_index_dict = {}
 
 # TODO: read brown_vocab_100.txt into word_index_dict
-
+with open("brown_vocab_100.txt", "r") as file:
+    count = 0
+    for line in file:
+        word_index_dict.setdefault(line.rstrip(), count)
+        count += 1
 # TODO: write word_index_dict to word_to_index_100.txt
-
-
+with open("word_to_index_100.txt", "w") as file:
+    file.write(str(word_index_dict))
 
 print(word_index_dict['all'])
 print(word_index_dict['resolution'])
