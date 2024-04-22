54735cd8ac9791e81a87c44bd20530e7c366153e
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2024-04-22 14:39:37 +0200

    removed to do

--- problem1.py
+++ problem1.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import nltk
-
 """
 NLP A2: N-Gram Language Models
 
@@ -11,13 +9,13 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 word_index_dict = {}
 
-# TODO: read brown_vocab_100.txt into word_index_dict
+# DONE: read brown_vocab_100.txt into word_index_dict
 with open("brown_vocab_100.txt", "r") as file:
     count = 0
     for line in file:
         word_index_dict.setdefault(line.rstrip(), count)
         count += 1
-# TODO: write word_index_dict to word_to_index_100.txt
+# DONE: write word_index_dict to word_to_index_100.txt
 with open("word_to_index_100.txt", "w") as file:
     file.write(str(word_index_dict))
 
